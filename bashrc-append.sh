79c5ae8d216ed04ea7453158ebeb228260e08cf1
Yangmun Choi <yangmungi@ucla.edu>
2012-10-04 13:17:51 -0700

    Moving ln commands to one time thing.

--- bashrc-append.sh
+++ bashrc-append.sh
@@ -1,5 +1,3 @@
 if [ -f $HOME/dot-home-dir/.bashrc ]; then
     . $HOME/dot-home-dir/.bashrc
-    ln -s $HOME/dot-home-dir/.vimrc 2> /dev/null
-    ln -s $HOME/dot-home-dir/.screenrc 2> /dev/null
 fi
--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 if [ -a $HOME/dot-home-dir/bashrc-append.sh ]; then
     /bin/cat $HOME/dot-home-dir/bashrc-append.sh >> $HOME/.bashrc
+
+    ln -s $HOME/dot-home-dir/.vimrc 2> /dev/null
+    ln -s $HOME/dot-home-dir/.screenrc 2> /dev/null
 fi
+
