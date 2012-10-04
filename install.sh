149d75661291790d6016acfcaa3e038310adf7a3
Yangmun Choi <yangmungi@ucla.edu>
2012-10-04 13:45:12 -0700

    Making ln calls explicit.

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ if [ -a $HOME/dot-home-dir/bashrc-append.sh ]; then
         /bin/cat $HOME/dot-home-dir/bashrc-append.sh >> $HOME/.bashrc
     fi
 
-    ln -s $HOME/dot-home-dir/.vimrc     2> /dev/null
-    ln -s $HOME/dot-home-dir/.screenrc  2> /dev/null
-    ln -s $HOME/dot-home-dir/.toprc     2> /dev/null
+    ln -s $HOME/dot-home-dir/.vimrc     $HOME/.vimrc    2> /dev/null
+    ln -s $HOME/dot-home-dir/.screenrc  $HOME/.screenrc 2> /dev/null
+    ln -s $HOME/dot-home-dir/.toprc     $HOME/.toprc    2> /dev/null
 fi
 
