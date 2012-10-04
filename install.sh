558b3237f14b7aa6d0fe9c740ee1831a07910613
Yangmun Choi <yangmungi@ucla.edu>
2012-10-04 13:40:00 -0700

    Adding toprc and more lightweight install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 if [ -a $HOME/dot-home-dir/bashrc-append.sh ]; then
-    /bin/cat $HOME/dot-home-dir/bashrc-append.sh >> $HOME/.bashrc
+    if [ -z "$(grep dot-home-dir $HOME/.bashrc)" ]; then
+        /bin/cat $HOME/dot-home-dir/bashrc-append.sh >> $HOME/.bashrc
+    fi
 
-    ln -s $HOME/dot-home-dir/.vimrc 2> /dev/null
-    ln -s $HOME/dot-home-dir/.screenrc 2> /dev/null
+    ln -s $HOME/dot-home-dir/.vimrc     2> /dev/null
+    ln -s $HOME/dot-home-dir/.screenrc  2> /dev/null
+    ln -s $HOME/dot-home-dir/.toprc     2> /dev/null
 fi
 
